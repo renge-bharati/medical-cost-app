e9f634fe26c85985d17bb1ec4b5c5f6ecb3dd813
Bharati Renge <rengebharati@gmail.com>
2026-03-05 15:23:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pickle
 import numpy as np
 
 # Load the trained model
-with open("best_model.pkl(1)", "rb") as file:
+with open("best_model(1).pkl", "rb") as file:
     model = pickle.load(file)
 
 st.title("🏥 Medical Insurance Charges Predictor")
